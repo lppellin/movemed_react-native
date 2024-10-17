e5214d5d3e4c5bb81034ad7cc1e48d7b22397dc4
lppellin <thelucaspellin@gmail.com>
2024-10-17 20:33:39 -0300

    add header initial

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function Header() {
+
+    
+
+    return (
+        <SafeAreaView>
+            <Text>Header</Text>
+        </SafeAreaView>
+    )
+}
