ed33ef0a1b94b51e65eeff304f9bf6a2f85de62f
lppellin <thelucaspellin@gmail.com>
2024-10-16 22:45:13 -0300

    add home srcreen initial

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Login from './src/screens/Login';
+import Home from './src/screens/Home';
 
 const Stack = createStackNavigator()
 
@@ -18,7 +17,13 @@ export default function App() {
           options={{ headerShown: false }}
         />
 
-        
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+
+
 
       </Stack.Navigator>
 
---
+++ src/screens/Home.tsx
@@ -0,0 +1,36 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { CommonActions, NavigationProp } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Button, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home({ navigation }: { navigation: NavigationProp<any> }) {
+
+
+    const handleLogout = async () => {
+        await AsyncStorage.removeItem('userProfile');
+        await AsyncStorage.removeItem('userName');
+
+        console.log('user logged out');
+
+        navigation.dispatch(
+            CommonActions.reset({
+                index: 0,
+                routes: [{ name: 'Login' }],
+            })
+        );
+    };
+
+
+    return (
+        <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+            <StatusBar style="auto" />
+
+            <Text>Home</Text>
+
+            <Button title="logout" onPress={handleLogout} />
+
+        </SafeAreaView>
+    );
+}
